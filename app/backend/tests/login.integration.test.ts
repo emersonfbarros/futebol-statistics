2b51098daaf5b895c91f525801c5cb56f7efa119
emersonfbarros <emersonfb99@gmail.com>
2023-11-16 14:02:31 -0300

    improves formatting on login tests file

--- app/backend/tests/login.integration.test.ts
+++ app/backend/tests/login.integration.test.ts
@@ -11,127 +11,124 @@ chai.use(chaiHttp);
 
 const { expect, request } = chai;
 
-describe('App', function() {
-  afterEach(function() {
+describe('App', function () {
+  afterEach(function () {
     sinon.restore();
   });
 
-  it(
-    'POST "/login" with empty password or email should return error message with http code 400',
-    async function() {
-      mock.login.empty.forEach(async (input) => {
-        const { status, body } = await request(app).post('/login').send(input);
-        expect(status).to.be.equal(mock.httpStatus.badReq);
-        expect(body).to.be.deep.equal({ message: 'All fields must be filled' });
-      });
-    },
-  );
+  it('POST "/login" with empty password or email should return error message with http code 400', async function () {
+    mock.login.empty.forEach(async (input) => {
+      const { status, body } = await request(app).post('/login').send(input);
+      expect(status).to.be.equal(mock.httpStatus.badReq);
+      expect(body).to.be.deep.equal({ message: 'All fields must be filled' });
+    });
+  });
 
-  it(
-    'POST "/login" with invalid password or email should return error message with http code 401',
-    async function() {
-      mock.login.invalid.forEach(async (input) => {
-        const { status, body } = await request(app).post('/login').send(input);
-        expect(status).to.be.equal(mock.httpStatus.unauthorized);
-        expect(body).to.be.deep.equal({ message: 'Invalid email or password' });
-      });
-    },
-  );
-
-  it(
-    'POST "/login" with valid password, but incorrect, and valid email found in db should return error message with http code 401',
-    async function() {
-      const { id, username, role, email, password } = mock.login.userOnDb;
-      const mockUserFound = UserModel.build({ id, username, role, email, password });
-
-      sinon.stub(UserModel, 'findOne').resolves(mockUserFound);
-
-      const { status, body } = await request(app)
-        .post('/login')
-        .send({ email, password: mock.login.invalid[1].password });
-
+  it('POST "/login" with invalid password or email should return error message with http code 401', async function () {
+    mock.login.invalid.forEach(async (input) => {
+      const { status, body } = await request(app).post('/login').send(input);
       expect(status).to.be.equal(mock.httpStatus.unauthorized);
       expect(body).to.be.deep.equal({ message: 'Invalid email or password' });
-    },
-  );
+    });
+  });
 
-  it(
-    'POST "/login" with valid password and valid email but not found in db should return error message with http code 401',
-    async function() {
-      const { email, password } = mock.login.userOnDb;
+  it('POST "/login" with valid password, but incorrect, and valid email found in db should return error message with http code 401', async function () {
+    const { id, username, role, email, password } = mock.login.userOnDb;
+    const mockUserFound = UserModel.build({
+      id,
+      username,
+      role,
+      email,
+      password,
+    });
 
-      sinon.stub(UserModel, 'findOne').resolves(null);
+    sinon.stub(UserModel, 'findOne').resolves(mockUserFound);
 
-      const { status, body } = await request(app)
-        .post('/login')
-        .send({ email, password });
+    const { status, body } = await request(app)
+      .post('/login')
+      .send({ email, password: mock.login.invalid[1].password });
 
-      expect(status).to.be.equal(mock.httpStatus.unauthorized);
-      expect(body).to.be.deep.equal({ message: 'Invalid email or password' });
-    },
-  );
+    expect(status).to.be.equal(mock.httpStatus.unauthorized);
+    expect(body).to.be.deep.equal({ message: 'Invalid email or password' });
+  });
 
-  it(
-    'GET "/login/role" without token in authorization header should return error message with http code 401',
-    async function() {
-      const { status, body } = await request(app)
-        .get('/login/role')
-        .set('authorization', 'Bearer ');
-      expect(status).to.be.equal(mock.httpStatus.unauthorized);
-      expect(body).to.be.deep.equal({ message: 'Token not found' });
-    },
-  );
+  it('POST "/login" with valid password and valid email but not found in db should return error message with http code 401', async function () {
+    const { email, password } = mock.login.userOnDb;
 
-  it(
-    'GET "/login/role" without invalid token in authorization header should return error message with http code 401',
-    async function() {
-      const { status, body } = await request(app)
-        .get('/login/role')
-        .set('authorization', `Bearer ${mock.login.invalidToken}`);
-      expect(status).to.be.equal(mock.httpStatus.unauthorized);
-      expect(body).to.be.deep.equal({ message: 'Token must be a valid token' });
-    },
-  );
+    sinon.stub(UserModel, 'findOne').resolves(null);
 
-  it(
-    'POST "/login" with valid email and password found in db should retun a token with http code 200',
-    async function() {
-      const { id, username, role, email, password, rawPassword } = mock.login.userOnDb;
-      const mockUserFound = UserModel.build({ id, username, role, email, password });
+    const { status, body } = await request(app)
+      .post('/login')
+      .send({ email, password });
 
-      const jwtSignStub = (sinon.stub(jwt, 'sign') as unknown as sinon.SinonStub<
+    expect(status).to.be.equal(mock.httpStatus.unauthorized);
+    expect(body).to.be.deep.equal({ message: 'Invalid email or password' });
+  });
+
+  it('GET "/login/role" without token in authorization header should return error message with http code 401', async function () {
+    const { status, body } = await request(app)
+      .get('/login/role')
+      .set('authorization', 'Bearer ');
+    expect(status).to.be.equal(mock.httpStatus.unauthorized);
+    expect(body).to.be.deep.equal({ message: 'Token not found' });
+  });
+
+  it('GET "/login/role" without invalid token in authorization header should return error message with http code 401', async function () {
+    const { status, body } = await request(app)
+      .get('/login/role')
+      .set('authorization', `Bearer ${mock.login.invalidToken}`);
+    expect(status).to.be.equal(mock.httpStatus.unauthorized);
+    expect(body).to.be.deep.equal({ message: 'Token must be a valid token' });
+  });
+
+  it('POST "/login" with valid email and password found in db should retun a token with http code 200', async function () {
+    const { id, username, role, email, password, rawPassword } =
+      mock.login.userOnDb;
+    const mockUserFound = UserModel.build({
+      id,
+      username,
+      role,
+      email,
+      password,
+    });
+
+    const jwtSignStub = (
+      sinon.stub(jwt, 'sign') as unknown as sinon.SinonStub<
         [{ id: number }, Secret],
         string
-      >).returns(mock.login.validToken);
-      sinon.stub(UserModel, 'findOne').resolves(mockUserFound);
+      >
+    ).returns(mock.login.validToken);
+    sinon.stub(UserModel, 'findOne').resolves(mockUserFound);
 
-      const { status, body } = await request(app)
-        .post('/login')
-        .send({ email, password: rawPassword });
+    const { status, body } = await request(app)
+      .post('/login')
+      .send({ email, password: rawPassword });
 
-      const jwtSignArgs = jwtSignStub.getCall(0).args;
+    const jwtSignArgs = jwtSignStub.getCall(0).args;
 
-      expect(status).to.be.equal(mock.httpStatus.successful);
-      expect(jwtSignArgs[0]).to.be.deep.equal({ id });
-      expect(jwtSignArgs[1]).to.be.a('string');
-      expect(body).to.be.deep.equal({ token: mock.login.validToken });
-    },
-  );
+    expect(status).to.be.equal(mock.httpStatus.successful);
+    expect(jwtSignArgs[0]).to.be.deep.equal({ id });
+    expect(jwtSignArgs[1]).to.be.a('string');
+    expect(body).to.be.deep.equal({ token: mock.login.validToken });
+  });
 
-  it(
-    'GET "/login/role" with valid email and password found in db should retun user role with http code 200',
-    async function() {
-      const { id, username, role, email, password } = mock.login.userOnDb;
-      const mockUserFound = UserModel.build({ id, username, role, email, password });
+  it('GET "/login/role" with valid email and password found in db should retun user role with http code 200', async function () {
+    const { id, username, role, email, password } = mock.login.userOnDb;
+    const mockUserFound = UserModel.build({
+      id,
+      username,
+      role,
+      email,
+      password,
+    });
 
-      sinon.stub(UserModel, 'findByPk').resolves(mockUserFound);
+    sinon.stub(UserModel, 'findByPk').resolves(mockUserFound);
 
-      const { status, body } = await request(app)
-        .get('/login/role')
-        .set('authorization', `Bearer ${mock.login.validToken}`)
+    const { status, body } = await request(app)
+      .get('/login/role')
+      .set('authorization', `Bearer ${mock.login.validToken}`);
 
-      expect(status).to.be.equal(mock.httpStatus.successful);
-      expect(body).to.be.deep.equal({ role });
-    },
-  );
-})
+    expect(status).to.be.equal(mock.httpStatus.successful);
+    expect(body).to.be.deep.equal({ role });
+  });
+});
