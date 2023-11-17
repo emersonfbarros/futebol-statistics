da564637b31f707982a0223aca1ca60fe881924e
emersonfbarros <emersonfb99@gmail.com>
2023-11-17 11:33:19 -0300

    adds UNAUTHORIZED status for service response

--- app/backend/src/types/ServiceResponse.ts
+++ app/backend/src/types/ServiceResponse.ts
@@ -1,6 +1,10 @@
 type ServiceMessage = { message: string };
 
-export type ServiceResponseStatus = 'SUCCESSFUL' | 'SERVER_ERROR' | 'NOT_FOUND';
+export type ServiceResponseStatus =
+  | 'SUCCESSFUL'
+  | 'SERVER_ERROR'
+  | 'NOT_FOUND'
+  | 'UNAUTHORIZED';
 
 export type ServiceResponse<T> = {
   status: ServiceResponseStatus;
