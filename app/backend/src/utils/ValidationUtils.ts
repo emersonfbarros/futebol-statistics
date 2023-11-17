3498f23685e794e1294eb4e4fac872b4620dac53
emersonfbarros <emersonfb99@gmail.com>
2023-11-16 21:11:50 -0300

    creates validation schemas for login data

---
+++ app/backend/src/utils/ValidationUtils.ts
@@ -0,0 +1,19 @@
+import * as joi from 'joi';
+import { LoginData } from '../types/LoginData';
+
+export default class ValidationUtils {
+  public static validateLoginSchema(data: LoginData): joi.ValidationResult {
+    const schema = joi
+      .object({
+        email: joi.string().empty('').email().required(),
+        password: joi.string().empty('').min(6).required(),
+      })
+      .messages({
+        'string.required': 'All fields must be filled',
+        'string.empty': 'All fields must be filled',
+        'string.email': 'Invalid email or password',
+        'string.min': 'Invalid email or password',
+      });
+    return schema.validate(data);
+  }
+}
