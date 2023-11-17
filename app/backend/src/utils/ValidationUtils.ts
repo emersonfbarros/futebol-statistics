a1dd199b9035403efb526b1a0534b87093154b2b
emersonfbarros <emersonfb99@gmail.com>
2023-11-17 12:50:31 -0300

    fixes joi error messages

--- app/backend/src/utils/ValidationUtils.ts
+++ app/backend/src/utils/ValidationUtils.ts
@@ -9,10 +9,10 @@ export default class ValidationUtils {
         password: joi.string().empty('').min(6).required(),
       })
       .messages({
-        'string.required': 'All fields must be filled',
-        'string.empty': 'All fields must be filled',
-        'string.email': 'Invalid email or password',
-        'string.min': 'Invalid email or password',
+        'any.required': 'All fields must be filled',
+        'any.empty': 'All fields must be filled',
+        'any.email': 'Invalid email or password',
+        'any.min': 'Invalid email or password',
       });
     return schema.validate(data);
   }
