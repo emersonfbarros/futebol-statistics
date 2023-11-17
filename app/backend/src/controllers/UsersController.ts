ffaa209a81bd8052c4a0a5ebbd55c6ea5fe38869
emersonfbarros <emersonfb99@gmail.com>
2023-11-17 18:25:35 -0300

    fixes name of usersSerice attribute

--- app/backend/src/controllers/UsersController.ts
+++ app/backend/src/controllers/UsersController.ts
@@ -4,10 +4,16 @@ import { LoginData } from '../types/LoginData';
 import Defaults from '../utils/Defaults';
 
 export default class UsersController {
-  private usersSerive = new UsersService();
+  private usersService = new UsersService();
 
   public async login({ body }: Request, res: Response) {
-    const { status, data } = await this.usersSerive.login(body as unknown as LoginData);
+    const { status, data } = await this.usersService.login(body as unknown as LoginData);
+    res.status(Defaults.getHttpCode(status)).json(data);
+  }
+
+  public async role(_req: Request, res: Response) {
+    console.log(res.locals.id);
+    const { status, data } = await this.usersService.role(Number(res.locals.id));
     res.status(Defaults.getHttpCode(status)).json(data);
   }
 }
