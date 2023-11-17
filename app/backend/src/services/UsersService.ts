e6b43c2b00a3bafa9013970912f0bea67e74864d
emersonfbarros <emersonfb99@gmail.com>
2023-11-17 12:08:47 -0300

    creates login method to UsersService class

---
+++ app/backend/src/services/UsersService.ts
@@ -0,0 +1,23 @@
+import AuthManager from '../auth/AuthManager';
+import UsersModel from '../models/UserModel';
+import { LoginData } from '../types/LoginData';
+import { ServiceResponse } from '../types/ServiceResponse';
+
+export default class UsersService {
+  private usersModel = new UsersModel();
+
+  public async login({ email, password }: LoginData): Promise<
+    ServiceResponse<{ token: string }>
+  > {
+    const user = await this.usersModel.findByEmail(email);
+    if (!user) {
+      return { status: 'UNAUTHORIZED', data: { message: 'Invalid email or password' } };
+    }
+    const passwordMatch = AuthManager.comparePassword(password, user.password);
+    if (!passwordMatch) {
+      return { status: 'UNAUTHORIZED', data: { message: 'Invalid email or password' } };
+    }
+    const token = AuthManager.getInstance().createToken({ id: user.id });
+    return { status: 'SUCCESSFUL', data: { token } };
+  }
+}
