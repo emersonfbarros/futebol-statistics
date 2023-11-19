268d5d67dddb63b5a1e647f84a0afc857d73477a
emersonfbarros <emersonfb99@gmail.com>
2023-11-19 13:49:36 -0300

    adds findTwoTeams method on TeamsModel class

--- app/backend/src/Interfaces/teams/ITeamModel.ts
+++ app/backend/src/Interfaces/teams/ITeamModel.ts
@@ -3,4 +3,5 @@ import ITeam from './ITeam';
 export default interface ITeamModel {
   findAll(): Promise<ITeam[]>;
   findById(id: number): Promise<ITeam | null>;
+  findTwoTeams(homeTeamId: number, awayTeamId: number): Promise<ITeam[]>;
 }
--- app/backend/src/models/TeamsModel.ts
+++ app/backend/src/models/TeamsModel.ts
@@ -1,3 +1,4 @@
+import { Op } from 'sequelize';
 import SequelizeTeams from '../database/models/TeamsModel';
 import ITeam from '../Interfaces/teams/ITeam';
 import ITeamModel from '../Interfaces/teams/ITeamModel';
@@ -13,5 +14,12 @@ export default class TeamsModel implements ITeamModel {
   async findById(id: number): Promise<ITeam | null> {
     const teamById = await this.model.findByPk(id);
     return teamById;
+  }
+
+  async findTwoTeams(homeTeamId: number, awayTeamId: number): Promise<ITeam[]> {
+    const foundTeams = await this.model.findAll({
+      where: { id: { [Op.or]: [homeTeamId, awayTeamId] } },
+    });
+    return foundTeams;
   }
 }
