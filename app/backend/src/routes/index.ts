322e5fcfc943b92ef19f4cc1219a2acb79657279
emersonfbarros <emersonfb99@gmail.com>
2023-11-18 21:09:22 -0300

    adds MatchesRoutes to MainRouter

--- app/backend/src/routes/index.ts
+++ app/backend/src/routes/index.ts
@@ -1,16 +1,19 @@
 import { Router } from 'express';
 import TeamsRoutes from './TeamsRoutes';
 import UsersRoutes from './UsersRoutes';
+import MatchesRoutes from './MatchesRoutes';
 
 export default class MainRouter {
   private router: Router;
   private teamsRouter: TeamsRoutes;
   private usersRouter: UsersRoutes;
+  private matchesRoute: MatchesRoutes;
 
   constructor() {
     this.router = Router();
     this.teamsRouter = new TeamsRoutes();
     this.usersRouter = new UsersRoutes();
+    this.matchesRoute = new MatchesRoutes();
 
     this.initializeRoutes();
   }
@@ -18,6 +21,7 @@ export default class MainRouter {
   private initializeRoutes() {
     this.router.use('/teams', this.teamsRouter.getRouter());
     this.router.use('/login', this.usersRouter.getRouter());
+    this.router.use('/matches', this.matchesRoute.getRouter());
   }
 
   public getRouter() {
