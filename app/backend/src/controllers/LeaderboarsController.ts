0e1d32f35e6ed3df024fda4b7c791afef18d77ff
emersonfbarros <emersonfb99@gmail.com>
2023-11-20 12:23:10 -0300

    creates LeaderboardsRoutes class and first methods

---
+++ app/backend/src/controllers/LeaderboarsController.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from 'express';
+import LeaderboardService from '../services/LeaderboardsService';
+import Defaults from '../utils/Defaults';
+
+export default class LeaderboadsController {
+  private leaderboardsSerice = new LeaderboardService();
+
+  public async getHomeTeamPerformance(_req: Request, res: Response) {
+    const { status, data } = await this.leaderboardsSerice.getHomeTeamsPerformance();
+    res.status(Defaults.getHttpCode(status)).json(data);
+  }
+}
