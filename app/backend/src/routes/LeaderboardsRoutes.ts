726f2da6da71df5b53ca972f7505e64052df9948
emersonfbarros <emersonfb99@gmail.com>
2023-11-20 12:23:46 -0300

    creates LeaderboardsRoutes class and first methods

---
+++ app/backend/src/routes/LeaderboardsRoutes.ts
@@ -0,0 +1,23 @@
+import { Request, Response, Router } from 'express';
+import LeaderboadsController from '../controllers/LeaderboarsController';
+
+export default class LeaderboardRoutes {
+  private router: Router;
+  private leaderboardController: LeaderboadsController;
+
+  constructor() {
+    this.router = Router();
+    this.leaderboardController = new LeaderboadsController();
+
+    this.initializeRoutes();
+  }
+
+  private initializeRoutes() {
+    this.router.get('/home', (req: Request, res: Response) =>
+      this.leaderboardController.getHomeTeamPerformance(req, res));
+  }
+
+  public getRouter() {
+    return this.router;
+  }
+}
--- app/backend/src/routes/index.ts
+++ app/backend/src/routes/index.ts
@@ -2,18 +2,21 @@ import { Router } from 'express';
 import TeamsRoutes from './TeamsRoutes';
 import UsersRoutes from './UsersRoutes';
 import MatchesRoutes from './MatchesRoutes';
+import LeaderboardRoutes from './LeaderboardsRoutes';
 
 export default class MainRouter {
   private router: Router;
   private teamsRouter: TeamsRoutes;
   private usersRouter: UsersRoutes;
   private matchesRoute: MatchesRoutes;
+  private leaderboardRoute: LeaderboardRoutes;
 
   constructor() {
     this.router = Router();
     this.teamsRouter = new TeamsRoutes();
     this.usersRouter = new UsersRoutes();
     this.matchesRoute = new MatchesRoutes();
+    this.leaderboardRoute = new LeaderboardRoutes();
 
     this.initializeRoutes();
   }
@@ -22,6 +25,7 @@ export default class MainRouter {
     this.router.use('/teams', this.teamsRouter.getRouter());
     this.router.use('/login', this.usersRouter.getRouter());
     this.router.use('/matches', this.matchesRoute.getRouter());
+    this.router.use('/leaderboard', this.leaderboardRoute.getRouter());
   }
 
   public getRouter() {
